c342d63292604e53b0f0d04d71bf2552cdb82731
Linette Hsu <linette.hsu13@gmail.com>
2017-04-21 23:57:22 -0500

    adding files with bonus

--- cloze.js
+++ cloze.js
@@ -1,12 +1,13 @@
-function ClozeCard (text, cloze) {
+function ClozeCard(text, cloze) {
 	if (text.indexOf(cloze) === -1){
 		throw new Error ("Cloze deletion not found in input text.")
-	}
-	this.fullText = text;
-	this.cloze = cloze;
-	this.partial = text.replace(cloze, "...");
-	this.displayCard = function() {
-		console.log(this.fullText);
+		}
+	if (this instanceof ClozeCard) {
+		this.fullText = text;
+		this.cloze = cloze;
+		this.partial = text.replace(cloze, "...");
+	} else {
+		return new ClozeCard(text, cloze)
 	}
 };
 
